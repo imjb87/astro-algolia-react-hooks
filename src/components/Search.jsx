2824a94c1048ebd6505ba8133ed681db64af1486
imjb87 <46000619+imjb87@users.noreply.github.com>
2023-02-27 23:35:04 +0000

    1 file modified

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -147,7 +147,15 @@ function CustomPagination(props) {
   } = usePagination(props);
   return (
     <div class="flex items-center justify-between border-t border-gray-200 bg-white py-3 mt-5">
-      <div class="hidden sm:flex sm:flex-1 sm:items-center sm:justify-center">
+      <div class="hidden sm:flex sm:flex-1 sm:items-center sm:justify-between">
+        <div>
+          <p class="text-sm text-gray-700 flex gap-x-1">
+            Showing
+            <span class="font-medium">{currentRefinement + 1}</span>
+            of
+            <span class="font-medium">{nbPages} page(s)</span>
+          </p>
+        </div>
         <div>
           <nav
             class="isolate inline-flex -space-x-px rounded-md shadow-sm"
@@ -155,6 +163,10 @@ function CustomPagination(props) {
           >
             <a
               href="#"
+              onClick={(event) => {
+                event.preventDefault();
+                refine(currentRefinement - 1);
+              }}
               class="relative inline-flex items-center rounded-l-md border border-gray-300 bg-white px-2 py-2 text-sm font-medium text-gray-500 hover:bg-gray-50 focus:z-20"
             >
               <span class="sr-only">Previous</span>
@@ -189,6 +201,10 @@ function CustomPagination(props) {
             ))}
             <a
               href="#"
+              onClick={(event) => {
+                event.preventDefault();
+                refine(currentRefinement + 1);
+              }}
               class="relative inline-flex items-center rounded-r-md border border-gray-300 bg-white px-2 py-2 text-sm font-medium text-gray-500 hover:bg-gray-50 focus:z-20"
             >
               <span class="sr-only">Next</span>
